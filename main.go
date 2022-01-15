0e679562872464edf63957c99d02ac9e4ba8bcfb
Róisín Grannell <r.grannell2@gmail.com>
2022-01-15 20:24:48 +0000

    pathed yaml escaping

--- main.go
+++ main.go
@@ -7,11 +7,11 @@ import (
 	"github.com/docopt/docopt-go"
 
 	_ "github.com/mattn/go-sqlite3"
-	opal "github.com/rgrannell1/opal"
+	opal "github.com/rgrannell1/opal/pkg"
 )
 
 func main() {
-	opts, err := docopt.ParseDoc(Usage())
+	opts, err := docopt.ParseDoc(opal.Usage())
 	if err != nil {
 		fmt.Printf("%+v\n", err)
 		os.Exit(1)
@@ -20,9 +20,9 @@ func main() {
 	fpath, _ := opts.String("<fpath>")
 
 	err = opal.Opal(&opal.OpalArgs{
-		fpath,
-		false,
-		true,
+		Fpath: fpath,
+		Audit: false,
+		Fix:   true,
 	})
 
 	if err != nil {
